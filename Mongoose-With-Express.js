9b95ad7093ad536af57a8a5c0f409d01d9c14d21
Madhav P <madhavp2023@gmail.com>
2025-05-21 06:24:39 +0530

    Learning Phase-1 Completed

--- Mongoose-With-Express.js
+++ Mongoose-With-Express.js
@@ -1,39 +1,51 @@
-import mongoose from "mongoose";
+// Importing required modules
+import mongoose from "mongoose";  1 // Mongoose is used to interact with MongoDB
+import express from "express";    // Express is a web framework for Node.js
 
-import express from "express";
+import { ToDo } from "./models/ToDo.js"; // Importing the ToDo model from the models folder
 
-import {ToDo} from "./models/ToDo.js"
-
+// Connecting to the local MongoDB server
 const conn = await mongoose.connect("mongodb://localhost:27017/");
 
-// const express = require('express')
+// Initializing the express application
+const app = express();
 
-const app = express()
-
-const port =3000
-
-app.get('/' ,(req,res) =>{
-
-   const todo = new ToDo({desc : "Description of this ToDo" , isDone : false , days : Math.floor(Math.random() * 45 + 5 * Math.random())})
-
-   
-   todo.save()
-    res.end('Hello World !')
-})
+// Defining the port number
+const port = 3000;
 
 
-app.get('/a'  , async (req,res) =>{
+// Route :-  Root endpoint
 
-   let todo =  await ToDo.findOne({})
-   console.log(todo);
+// Creates and saves a new ToDo item to the database
+app.get("/", (req, res) => {
+  const todo = new ToDo({
+    desc: "Description of this ToDo",    // Description of the task
+    isDone: false,                 // Marked as not done initially
+    days: Math.floor(Math.random() * 45 + 5 * Math.random()),      // Random number of days (somewhat randomized)
+  });
 
-   if(!todo){
-    return res.status(404).json({ error : "No ToDo found here "})
-   }
-   res.json({title : todo.title,desc : todo.desc})
-})
+  todo.save();           // Save the new ToDo to the MongoDB collection
+  res.end("Hello World !");      // Send a simple response
+});
 
 
-app.listen(port , () =>[
-    console.log(`Example app listening on port ${port}`)
-])
+
+// Route: /a endpoint
+// Fetches a ToDo item from the database and returns it
+app.get("/a", async (req, res) => {
+  let todo = await ToDo.findOne({}); // Find any one ToDo item from the collection
+  console.log(todo);        // Log it to the console for debugging
+
+  // If no ToDo is found, return a 404 error response
+  if (!todo) {
+    return res.status(404).json({ error: "No ToDo found here " });
+  }
+
+
+  // If found, return selected fields as JSON
+  res.json({ title: todo.title, desc: todo.desc });
+});
+
+
+// Start the Express server and listen on the defined port
+app.listen(port, () => [console.log(`Example app listening on port ${port}`)]);
--- models/ToDo.js
+++ models/ToDo.js
@@ -1,14 +1,19 @@
-import mongoose from "mongoose"
+// Import mongoose to define the schema and model
+import mongoose from "mongoose";
 
 
-// Creation of a Database 
+// Define a schema for the ToDo collection in MongoDB
 const ToDoSchema = new mongoose.Schema({
-    title : {type : String , required : true , default : "Key"},
-    desc : String,
-    isDone : Boolean,
-    days : Number
-
-
+  title: {
+    type: String,      // Title must be a string
+    required: true,    // Title is mandatory
+    default: "Key",    // Default title if none is provided
+  },
+  desc: String,     // Description of the ToDo (optional)
+  isDone: Boolean,  // Status of completion (true/false)
+  days: Number,     // Number of days (can be used for due date or tracking)
 });
 
-export const ToDo = mongoose.model('ToDo' , ToDoSchema)
+
+// Export the model so it can be used in other files
+export const ToDo = mongoose.model("ToDo", ToDoSchema);
